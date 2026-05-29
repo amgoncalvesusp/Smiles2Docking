97a31b4adfd97ec153a8fb70321e3b836b330d11
Adriano Marques Goncalves <adrianogoncalves.bio@gmail.com>
2026-05-28 21:12:02 -0300

    fix: add AUTHOR_SUMMARY to app_metadata to fix GUI startup crash
    main_window imported AUTHOR_SUMMARY which was undefined, causing
    ImportError on launch from the installed build. Define AUTHORS tuple
    (all three authors) and derive AUTHOR_SUMMARY from it.

--- src/app_metadata.py
+++ src/app_metadata.py
@@ -5,4 +5,10 @@ APP_VERSION = "1.2"
 AUTHOR_NAME = "Adriano Marques Gonçalves"
 AUTHOR_AFFILIATION = "Universidade de Araraquara - UNIARA"
 AUTHOR_EMAIL = "amgoncalves@uniara.edu.br"
+AUTHORS = (
+    "Adriano Marques Gonçalves (UNIARA)",
+    "Daniel Grajales Ruiz (UNESP)",
+    "Nailton Monteiro do Nascimento Júnior (UNESP)",
+)
+AUTHOR_SUMMARY = "; ".join(AUTHORS)
 PROJECT_LICENSE = "GPL-2.0-or-later"
